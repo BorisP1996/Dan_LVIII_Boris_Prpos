2e4625a54df2c1b3949f931d5ea16ab38d97600c
BorisP1996 <prposboris13@hotmail.com>
2020-08-31 13:13:29 +0200

    added functionalities

--- Zadatak_1/MainWindow.xaml.cs
+++ Zadatak_1/MainWindow.xaml.cs
@@ -25,9 +25,96 @@ namespace Zadatak_1
             InitializeComponent();
         }
 
-        private void But1_Click()
-        {
+        int turn;
+        int xCounter;
+        int oCounter;
+        int drawCounter;
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            turn = 1;
         }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Button btn in WrapPanel.Children)
+            {
+                btn.Content = "";
+                btn.IsEnabled = true;
+
+            }
+        }
+
+        private void Win(string btnContent)
+        {
+            if ((But1.Content == btnContent & But2.Content == btnContent &
+                 But3.Content == btnContent)
+               | (But1.Content == btnContent & But4.Content == btnContent &
+                 But7.Content == btnContent)
+               | (But1.Content == btnContent & But5.Content == btnContent &
+                 But9.Content == btnContent)
+               | (But2.Content == btnContent & But5.Content == btnContent &
+                 But8.Content == btnContent)
+               | (But3.Content == btnContent & But6.Content == btnContent &
+                 But9.Content == btnContent)
+               | (But4.Content == btnContent & But5.Content == btnContent &
+                 But6.Content == btnContent)
+               | (But7.Content == btnContent & But8.Content == btnContent &
+                 But9.Content == btnContent)
+               | (But3.Content == btnContent & But5.Content == btnContent &
+                 But7.Content == btnContent))
+            {
+                if (btnContent == "O")
+                {
+
+                    MessageBox.Show("Player O has won the game.");
+                    oWins.Content = ++xCounter;
+                }
+                else if (btnContent == "X")
+                {
+                    MessageBox.Show("Player X has won the game.");
+                    xWins.Content = ++oCounter;
+                }
+                disablebuttons();
+            }
+
+            else
+            {
+                foreach (Button btn in WrapPanel.Children)
+                {
+                    if (btn.IsEnabled == true)
+                        return;
+                }
+                MessageBox.Show("Draw game.");
+                drawCounterLabel.Content = ++drawCounter;
+            }
+        }
+        private void disablebuttons()
+        {
+            foreach (Button btn in WrapPanel.Children)
+            {
+                btn.IsEnabled = false;
+            }
+        }
+        private void But_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (turn == 1)
+            {
+                btn.Content = "O";
+               nextMove.Content = "X";
+            }
+            else
+            {
+                btn.Content = "X";
+                nextMove.Content = "O";
+            }
+            btn.IsEnabled = false;
+            Win(btn.Content.ToString());
+            turn += 1;
+            if (turn > 2)
+                turn = 1;
+        }
+
     }
 }
